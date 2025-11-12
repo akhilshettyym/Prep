d47f4d1b9eaaade5b2df9eefc87f45707be0fd08
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-11-12 23:58:05 +0530

    Working commit

--- gec/babel.config.js
+++ gec/babel.config.js
@@ -1,7 +1,24 @@
-module.exports = function (api) {
-  api.cache(true);
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: ['inline-dotenv'],
+//   };
+// };
+
+
+// module.exports = (api) => {
+//   api.cache(true)
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: ["inline-dotenv"],
+//   }
+// }
+
+module.exports = (api) => {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['inline-dotenv'],
-  };
-};
+    presets: ["babel-preset-expo"],
+    plugins: ["inline-dotenv", ["@babel/plugin-transform-react-compiler", { target: "18" }]],
+  }
+}
